53ae176629917235e12b1e3e7fd760937ef2e741
Rui Iwabuchi <rui.iwabuchi@cybertech.co.jp>
2024-09-17 13:13:51 +0800

    enum concept

---
+++ AdvJava/Main.java
@@ -0,0 +1,63 @@
+package AdvJava;
+
+
+//enums is good for using switch statement
+public class Main {
+
+    public static void main(String[] args) {
+        Status[] ss = Status.values();
+        for(Status s: ss){
+            System.out.println(s.ordinal() + " : " + s );
+        }
+
+
+        Status s = Status.Failed;
+
+        // switch (s) {
+        //     case Running:
+        //             System.out.println("Running");
+        //         break;
+        //     case Failed:
+        //             System.out.println("Failed");
+        //         break;
+        //     case Success:
+        //             System.out.println("Success");
+        //         break;
+
+        // }
+        switch (s) {
+            case Running -> System.out.println("Running");
+            case Failed -> System.out.println("Failed");
+            case Success -> System.out.println("Success");
+        }
+
+        for(Fruits fruit : Fruits.values()){
+            System.out.println(fruit + " : " + fruit.getPrice());
+        }
+
+    }
+}
+
+enum Status{
+    Running, Failed, Success
+}
+
+enum Fruits{
+    Mango(20), Apple(22), Orange(15);
+
+    int price;
+
+    Fruits(int price) {
+      this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
